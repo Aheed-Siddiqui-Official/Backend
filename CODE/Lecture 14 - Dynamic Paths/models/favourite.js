0e2f13740cdbf936a6af862a5593845543cbfc9f
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-11-03 18:49:30 +0500

    commit

--- CODE/Lecture 14 - Dynamic Paths/models/favourite.js
+++ CODE/Lecture 14 - Dynamic Paths/models/favourite.js
@@ -6,7 +6,6 @@ const rootDir = require("../utils/pathUtil");
 const favouriteDataPath = path.join(rootDir, "data", "favourite.json");
 
 module.exports = class Favourite {
-
   static addToFavourite(homeId, callback, error) {
     Favourite.getFavourites((favourites) => {
       if (favourites.includes(homeId)) {
@@ -22,4 +21,12 @@ module.exports = class Favourite {
     fs.readFile(favouriteDataPath, (err, data) => {
       callback(!err ? JSON.parse(data) : []);
     });
-  }}
+  }
+
+  static deleteById(delHomeId, callback) {
+    Favourite.getFavourites((homeIds) => {
+      homeIds = homeIds.filter((homeId) => delHomeId !== homeId);
+      fs.writeFile(favouriteDataPath, JSON.stringify(homeIds), callback);
+    });
+  }
+};
---
+++ CODE/Lecture 15 - Intoruction to SQL/controllers/hostController.js
@@ -0,0 +1,88 @@
+const Home = require("../models/home");
+
+exports.getAddHome = (req, res, next) => {
+  res.render("host/edit-home", {
+    pageTitle: "Add Home to airbnb",
+    currentPage: "addHome",
+    editing: false,
+  });
+};
+
+exports.getEditHome = (req, res, next) => {
+  const homeId = req.params.homeId;
+  const editing = req.query.editing === "true";
+
+  Home.findById(homeId).then(([homes]) => {
+    const home = homes[0];
+    if (!home) {
+      console.log("Home not found for editing.");
+      return res.redirect("/host/host-home-list");
+    }
+    res.render("host/edit-home", {
+      home: home,
+      pageTitle: "Edit your Home",
+      currentPage: "host-homes",
+      editing: editing,
+    });
+  });
+};
+
+exports.getHostHomes = (req, res, next) => {
+  Home.fetchAll().then(([registeredHomes]) => {
+    res.render("host/host-home", {
+      registeredHomes: registeredHomes,
+      pageTitle: "Host Home List",
+      currentPage: "host-home",
+    });
+  });
+};
+
+exports.postAddHome = (req, res, next) => {
+  const { houseName, price, location, rating, photoUrl, description } =
+    req.body;
+  const home = new Home(
+    houseName,
+    price,
+    location,
+    rating,
+    photoUrl,
+    description
+  );
+  home.save();
+
+  res.redirect("/host/host-home-list");
+};
+
+exports.postEditHome = (req, res, next) => {
+  // new work
+  console.log("Home registration successful for:", req.body);
+  const { id, houseName, price, location, rating, photoUrl, description } =
+    req.body;
+
+  const home = new Home(
+    houseName,
+    price,
+    location,
+    rating,
+    photoUrl,
+    description,
+    id
+  );
+
+  home.save();
+
+  res.redirect("/host/host-home-list");
+};
+
+exports.postDeleteHome = (req, res, next) => {
+  const homeId = req.params.homeId;
+  console.log("thi sis homeid", homeId);
+
+  Home.deleteById(homeId)
+    .then(() => {
+      res.redirect("/host/host-home-list");
+    })
+    .catch((error) => {
+      console.log("Error while deleting", error);
+    });
+};
