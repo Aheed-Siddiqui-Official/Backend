435192a2fa6026e1f9d6726bd851d418f6548b01
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-11-05 21:36:10 +0500

    commit

--- CODE/Lecture 17 - Introduction to Mongoose/app.js
+++ CODE/Lecture 17 - Introduction to Mongoose/app.js
@@ -9,7 +9,7 @@ const storeRouter = require("./routes/storeRouter");
 const hostRouter = require("./routes/hostRouter");
 const rootDir = require("./utils/pathUtil");
 const errorsController = require("./controllers/errors");
-const { mongoConnect } = require("./utils/dataBaseUtil");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 
@@ -26,8 +26,17 @@ app.use(express.static(path.join(rootDir, "public")));
 app.use(errorsController.pageNotFound);
 
 const PORT = 3000;
-mongoConnect(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on address http://localhost:${PORT}`);
+const DB_PATH = "mongodb+srv://mas:mas29@mas.lrkcwy5.mongodb.net/airbnb?appName=MAS";
+
+mongoose
+  .connect(DB_PATH)
+  .then(() => {
+    console.log("Connected to mongo");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on address http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error while connecting to Mongo", err);
   });
-});
