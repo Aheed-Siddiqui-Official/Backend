e2582a72d84e3186ab1697c9bac297e8726c0073
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-11-08 18:24:13 +0500

    commit

---
+++ CODE/Lecture 18 - Cookies and Sessions/app.js
@@ -0,0 +1,76 @@
+//core module
+const path = require("path");
+
+// external module
+const express = require("express");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const DB_PATH =
+  "mongodb+srv://mas:mas29@mas.lrkcwy5.mongodb.net/airbnb?appName=MAS";
+
+// local module
+const storeRouter = require("./routes/storeRouter");
+const hostRouter = require("./routes/hostRouter");
+const rootDir = require("./utils/pathUtil");
+const errorsController = require("./controllers/errors");
+const { default: mongoose } = require("mongoose");
+const authRouter = require("./routes/authRouter");
+
+const app = express();
+
+// new work
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const store = new MongoDBStore({
+  uri: DB_PATH,
+  collection: "sessions",
+});
+
+app.use(express.urlencoded());
+
+app.use(
+  session({
+    secret: "MERN",
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+  })
+);
+
+app.use((req, res, next) => {
+  req.isLoggedIn = req.session.isLoggedIn;
+  next();
+});
+
+app.use(authRouter);
+app.use(storeRouter);
+
+app.use("/host", (req, res, next) => {
+  if (req.isLoggedIn) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.use("/host", hostRouter);
+
+app.use(express.static(path.join(rootDir, "public")));
+
+app.use(errorsController.pageNotFound);
+
+const PORT = 3000;
+
+mongoose
+  .connect(DB_PATH)
+  .then(() => {
+    console.log("Connected to mongo");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on address http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error while connecting to Mongo", err);
+  });
