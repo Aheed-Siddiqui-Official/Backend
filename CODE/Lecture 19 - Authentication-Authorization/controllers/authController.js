1c6510252b13270cfa7e82634f53accf735f2bef
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-11-10 19:34:16 +0500

    commit

---
+++ CODE/Lecture 19 - Authentication-Authorization/controllers/authController.js
@@ -0,0 +1,40 @@
+exports.getLogin = (req, res, next) => {
+  res.render("auth/login", {
+    pageTitle: "Login",
+    currentPage: "login",
+    isLoggedIn: false,
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    pageTitle: "SignUp",
+    currentPage: "signup",
+    isLoggedIn: false,
+  });
+};
+
+exports.postSignup = (req, res, next) => {
+  console.log(req.body);
+
+  //req.session.isLoggedIn = true;
+  // res.cookie("isLoggedIn", true);
+  // req.isLoggedIn = true;
+  res.redirect("/");
+};
+
+
+exports.postLogin = (req, res, next) => {
+  console.log(req.body);
+
+  req.session.isLoggedIn = true;
+  // res.cookie("isLoggedIn", true);
+  // req.isLoggedIn = true;
+  res.redirect("/");
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(() => {
+    res.redirect("/login");
+  });
+};
