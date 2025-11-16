754bcadd3ba64dc69921cf02b00606e48593390a
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-11-16 15:02:54 +0500

    commit

--- CODE/Lecture 19 - Authentication-Authorization/controllers/authController.js
+++ CODE/Lecture 19 - Authentication-Authorization/controllers/authController.js
@@ -1,3 +1,6 @@
+const { check, validationResult } = require("express-validator");
+
+// GET LOGIN
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     pageTitle: "Login",
@@ -6,33 +9,107 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+// GET SIGNUP
 exports.getSignup = (req, res, next) => {
   res.render("auth/signup", {
-    pageTitle: "SignUp",
+    pageTitle: "Signup",
     currentPage: "signup",
     isLoggedIn: false,
+    errors: [],
+    oldInput: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      userType: "",
+    },
   });
 };
 
-exports.postSignup = (req, res, next) => {
-  console.log(req.body);
+// POST SIGNUP
+exports.postSignup = [
+  // VALIDATIONS
+  check("firstName")
+    .trim()
+    .isLength({ min: 2 })
+    .withMessage("First name should be at least 2 characters long")
+    .matches(/^[A-Za-z\s]+$/)
+    .withMessage("First name should contain only alphabets"),
 
-  //req.session.isLoggedIn = true;
-  // res.cookie("isLoggedIn", true);
-  // req.isLoggedIn = true;
-  res.redirect("/");
-};
+  check("lastName")
+    .matches(/^[A-Za-z\s]*$/)
+    .withMessage("Last name should contain only alphabets"),
 
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email")
+    .normalizeEmail(),
 
+  check("password")
+    .isLength({ min: 8 })
+    .withMessage("Password should be at least 8 characters long")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain at least one lowercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Password must contain at least one number")
+    .matches(/[!@&]/)
+    .withMessage("Password must contain at least one special character")
+    .trim(),
+
+  check("confirmPassword")
+    .trim()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords do not match");
+      }
+      return true;
+    }),
+
+  check("userType")
+    .notEmpty()
+    .withMessage("Please select a user type")
+    .isIn(["guest", "host"])
+    .withMessage("Invalid user type"),
+
+  check("terms")
+    .notEmpty()
+    .withMessage("Please accept the terms and conditions")
+    .custom((value) => {
+      if (value !== "on") {
+        throw new Error("Please accept the terms and conditions");
+      }
+      return true;
+    }),
+
+  // HANDLER
+  (req, res, next) => {
+    const { firstName, lastName, email, password, userType } = req.body;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).render("auth/signup", {
+        pageTitle: "Signup",
+        currentPage: "signup",
+        isLoggedIn: false,
+        errors: errors.array().map((err) => err.msg),
+        oldInput: { firstName, lastName, email, password, userType },
+      });
+    }
+
+    // TODO: ADD SAVING USER TO DB HERE
+
+    res.redirect("/login");
+  },
+];
+
+// POST LOGIN
 exports.postLogin = (req, res, next) => {
-  console.log(req.body);
-
   req.session.isLoggedIn = true;
-  // res.cookie("isLoggedIn", true);
-  // req.isLoggedIn = true;
   res.redirect("/");
 };
 
+// POST LOGOUT
 exports.postLogout = (req, res, next) => {
   req.session.destroy(() => {
     res.redirect("/login");
